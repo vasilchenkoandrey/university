7e1580ce66762f9fe73cdf129c4cb9361cd2e1b0
vasilchenkoandrey <71325160+vasilchenkoandrey@users.noreply.github.com>
2023-03-28 21:17:47 +0300

    Add files via upload

---
+++ Архитектура вычислительных сред/4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double x) {
+    return sqrt(1 - x * x);
+}
+
+double simpson(double a, double b) {
+    double c = (a + b) / 2.0;
+    return (f(a) + 4 * f(c) + f(b)) * (b - a) / 6.0;
+}
+
+double adaptive_simpson(double a, double b, double eps, double whole) {
+    double c = (a + b) / 2.0;
+    double left = simpson(a, c);
+    double right = simpson(c, b);
+    double result = left + right;
+
+    if (fabs(result - whole) <= 15 * eps) {
+        return result + (result - whole) / 15.0;
+    } else {
+        return adaptive_simpson(a, c, eps / 2.0, left) + adaptive_simpson(c, b, eps / 2.0, right);
+    }
+}
+
+int main() {
+    double a = 0.0, b = 1.0;
+    double eps = 1e-6;
+    double whole = simpson(a, b);
+
+    double result = adaptive_simpson(a, b, eps, whole);
+    double quarter_circle_area = result * 4.0;
+
+    cout << "The area of the quarter circle with radius 1 is: " << quarter_circle_area << endl;
+
+    return 0;
+}
