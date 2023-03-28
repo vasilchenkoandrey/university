302ff585ea8f8c3ddfce539e8cb541fe3b898093
vasilchenkoandrey <71325160+vasilchenkoandrey@users.noreply.github.com>
2023-03-28 17:31:18 +0300

    Update 2.cpp

--- Архитектура вычислительных сред/2.cpp
+++ Архитектура вычислительных сред/2.cpp
@@ -30,51 +30,37 @@ void multiply_matrices(int** A, int** B, int** C, int n, int start, int end) {
 }
 
 int main() {
-    int n = 500;
-    
-    int array_threads[] = {1, 2, 3, 4};
-    const int num_threads = sizeof(array_threads) / sizeof(int);
+    const int sizes[] = {10, 20, 50, 100, 200, 500, 1000};
+    const int num_sizes = sizeof(sizes) / sizeof(int);
 
-    cout << "Num threads\tTime taken (ms)\n";
+    cout << "Matrix Size\tTime taken (ms)\n";
     
-    // Initialize matrices
-    int** A = new int*[n];
-    init_matrix(A, n);
-    
-    int** B = new int*[n];
-    init_matrix(B, n);
-    
-    int** C = new int*[n];
-    init_matrix(C, n);
-    
-    vector<int> time_taken;
+    for(int i=0; i<num_sizes; i++) {
+        const int n = sizes[i];
         
-    for(int i=0; i<num_threads; i++) {
-
-        // Multiply matrices and measure time
-        double start_time = clock();
-
-        // Number of cores available
-        // const int num_threads = thread::hardware_concurrency();
+        int** A = new int*[n];
+        init_matrix(A, n);
         
-        auto start = chrono::system_clock::now();
-        vector<thread> threads;
-        for(int j=0; j<array_threads[i]; j++) {
-            const int block_size = n / array_threads[i];
-            int start = j * block_size;
-            int end = (j == array_threads[i] - 1) ? n : (j + 1) * block_size;
-            threads.emplace_back(multiply_matrices, A, B, C, n, start, end);
+        int** B = new int*[n];
+        init_matrix(B, n);
+        
+        int** C = new int*[n];
+        for(int j=0; j<n; j++) {
+            C[j] = new int[n];
         }
 
+        vector<thread> threads;
+        auto start = std::chrono::system_clock::now();
+        for(int i=0; i<n; i++) {
+            threads.emplace_back(multiply_matrices, A, B, C, n, i, i+1);
+        }
         for(auto& thread : threads) {
             thread.join();
         }
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
 
-        double end_time = clock();
-        auto end = chrono::system_clock::now();
-        chrono::duration<double> elapsed_seconds = end - start;
-     
-        cout << array_threads[i] << "\t\t" << ((end_time - start_time) / CLOCKS_PER_SEC) << "  " << elapsed_seconds.count() << endl;
+        cout << n << "x" << n << "\t\t" << elapsed_seconds.count() << endl;
     }
 
     return 0;
