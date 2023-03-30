e7696210d2fb8ab7a5034973fec7ce90a3ed3363
vasilchenkoandrey <71325160+vasilchenkoandrey@users.noreply.github.com>
2023-03-30 19:46:21 +0300

    Update 5.cpp

--- Архитектура вычислительных сред/5.cpp
+++ Архитектура вычислительных сред/5.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <future>
 #include <cmath>
-#include <vector>
+#include <thread>
 
 using namespace std;
 
@@ -9,45 +8,39 @@ double f(double x) {
     return sqrt(1 - x * x);
 }
 
-double simpson_integrate(double a, double b, double eps) {
-    double h = (b - a) / 2.0;
-    double fa = f(a);
-    double fb = f(b);
-    double fm = f((a + b) / 2.0);
-
-    double integral = h / 3.0 * (fa + 4.0 * fm + fb);
-
-    return integral;
+double simpson(double a, double b) {
+    double c = (a + b) / 2.0;
+    return (f(a) + 4 * f(c) + f(b)) * (b - a) / 6.0;
 }
 
-double integral(double eps, int num_threads) {
-    double a = 0.0;
-    double b = 1.0;
-    double h = (b - a) / num_threads;
+double adaptive_simpson(double a, double b, double eps, double whole) {
+    double c = (a + b) / 2.0;
+    double left, right;
+    thread left_thread, right_thread;
 
-    vector<future<double>> futures;
+    left_thread = thread([&]() { left = simpson(a, c); });
+    right_thread = thread([&]() { right = simpson(c, b); });
 
-    for (int i = 0; i < num_threads; ++i) {
-        double left = a + i * h;
-        double right = left + h;
+    left_thread.join();
+    right_thread.join();
 
-        futures.push_back(async(launch::async, simpson_integrate, left, right, eps));
+    double result = left + right;
+
+    if (fabs(result - whole) <= 15 * eps) {
+        return result + (result - whole) / 15.0;
+    } else {
+        return adaptive_simpson(a, c, eps / 2.0, left) + adaptive_simpson(c, b, eps / 2.0, right);
     }
-
-    double sum = 0.0;
-    for (int i = 0; i < num_threads; ++i) {
-        sum += futures[i].get();
-    }
-
-    return sum;
 }
 
 int main() {
+    double a = 0.0, b = 1.0;
     double eps = 1e-6;
-    int num_threads = 4;
+    double whole = simpson(a, b);
 
-    double res = integral(eps, num_threads);
-    cout << "Integral: " << res << endl;
+    double result = adaptive_simpson(a, b, eps, whole);
+
+    cout << "The area of the quarter circle with radius 1 is: " << result << endl;
 
     return 0;
 }
