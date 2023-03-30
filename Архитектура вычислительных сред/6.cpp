2a5f05a47a1ff8b38ba6b454d89de16232cc9650
vasilchenkoandrey <71325160+vasilchenkoandrey@users.noreply.github.com>
2023-03-30 17:58:08 +0300

    Update 6.cpp

--- Архитектура вычислительных сред/6.cpp
+++ Архитектура вычислительных сред/6.cpp
@@ -2,7 +2,7 @@
 #include <thread>
 #include <vector>
 #include <cmath>
-#include <atomic>
+#include <mutex>
 
 using namespace std;
 
@@ -15,7 +15,7 @@ double simpson(double a, double b) {
     return (f(a) + 4 * f(c) + f(b)) * (b - a) / 6.0;
 }
 
-double adaptive_simpson(double a, double b, double eps, double whole, atomic<int>& threads_count) {
+double adaptive_simpson(double a, double b, double eps, double whole, int& threads_count, mutex& mtx) {
     double c = (a + b) / 2.0;
     double left = simpson(a, c);
     double right = simpson(c, b);
@@ -24,38 +24,37 @@ double adaptive_simpson(double a, double b, double eps, double whole, atomic<int
     if (fabs(result - whole) <= 15 * eps) {
         return result + (result - whole) / 15.0;
     } else {
-        if (threads_count < 2) { // Ограничиваем максимальное количество потоков
-            double result_left = 0.0, result_right = 0.0;
+        double result_left = 0.0, result_right = 0.0;
+        thread thread_left([&](){
+            mtx.lock();
             threads_count++;
-            thread thread_left([&](){
-                result_left = adaptive_simpson(a, c, eps / 2.0, left, threads_count);
-            });
+            mtx.unlock();
+            result_left = adaptive_simpson(a, c, eps / 2.0, left, threads_count, mtx);
+        });
+        thread thread_right([&](){
+            mtx.lock();
             threads_count++;
-            thread thread_right([&](){
-                result_right = adaptive_simpson(c, b, eps / 2.0, right, threads_count);
-            });
-            thread_left.join();
-            thread_right.join();
-            return result_left + result_right;
-        } else { // Переходим к рекурсивному вычислению, если количество потоков превысило максимальное
-            double result_left = adaptive_simpson(a, c, eps / 2.0, left, threads_count);
-            double result_right = adaptive_simpson(c, b, eps / 2.0, right, threads_count);
-            return result_left + result_right;
-        }
+            mtx.unlock();
+            result_right = adaptive_simpson(c, b, eps / 2.0, right, threads_count, mtx);
+        });
+        thread_left.join();
+        thread_right.join();
+        return result_left + result_right;
     }
 }
 
 int main() {
-    double a = 0.0, b = 1;
+    double a = 0.0, b = 0.80;
     double eps = 1e-6;
     double whole = simpson(a, b);
 
-    atomic<int> threads_count(0);
+    int threads_count = 0;
+    mutex mtx;
 
-    double result = adaptive_simpson(a, b, eps, whole, threads_count);
+    double result = adaptive_simpson(a, b, eps, whole, threads_count, mtx);
 
     cout << "The area of the quarter circle with radius 1 is: " << result << endl;
-    
+    cout << "Threads count: " << threads_count << endl;
 
     return 0;
 }
