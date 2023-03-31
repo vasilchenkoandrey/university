74eba84b11897609e782002db31538f8e8b8f3a1
vasilchenkoandrey <71325160+vasilchenkoandrey@users.noreply.github.com>
2023-03-31 17:46:17 +0300

    Update 4.cpp

--- Архитектура вычислительных сред/4.cpp
+++ Архитектура вычислительных сред/4.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include <cmath>
+#include <thread>
 
 using namespace std;
 
@@ -13,16 +13,29 @@ double simpson(double a, double b) {
     return (f(a) + 4 * f(c) + f(b)) * (b - a) / 6.0;
 }
 
-double adaptive_simpson(double a, double b, double eps, double whole) {
+void adaptive_simpson(double a, double b, double eps, double whole, double& result) {
     double c = (a + b) / 2.0;
-    double left = simpson(a, c);
-    double right = simpson(c, b);
-    double result = left + right;
+
+    double left, right;
+    thread left_thread([=, &left]() { left = simpson(a, c); });
+    thread right_thread([=, &right]() { right = simpson(c, b); });
+
+    left_thread.join();
+    right_thread.join();
+
+    result = left + right;
 
     if (fabs(result - whole) <= 15 * eps) {
-        return result + (result - whole) / 15.0;
+        result += (result - whole) / 15.0;
     } else {
-        return adaptive_simpson(a, c, eps / 2.0, left) + adaptive_simpson(c, b, eps / 2.0, right);
+        double result1, result2;
+        thread thread1([=, &result1]() { adaptive_simpson(a, c, eps / 2.0, left, result1); });
+        thread thread2([=, &result2]() { adaptive_simpson(c, b, eps / 2.0, right, result2); });
+
+        thread1.join();
+        thread2.join();
+
+        result = result1 + result2;
     }
 }
 
@@ -30,8 +43,9 @@ int main() {
     double a = 0.0, b = 1.0;
     double eps = 1e-6;
     double whole = simpson(a, b);
+    double result = 0.0;
 
-    double result = adaptive_simpson(a, b, eps, whole);
+    adaptive_simpson(a, b, eps, whole, result);
 
     cout << "The area of the quarter circle with radius 1 is: " << result << endl;
 
